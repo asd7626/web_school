02e2ddee817924f0d64159eff4315a9e34d9c362
asd7626 <asdshq19@gmail.com>
2020-08-17 18:09:30 +0300

    SQLAlchemy

--- school.py
+++ school.py
@@ -1,20 +1,13 @@
 from flask import Flask, url_for, redirect, request, render_template, flash
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Length
 
-
 app = Flask(__name__)
-app.config.update(dict(
-    DEBUG=True,
-    SECRET_KEY='asd'
-))
-
-students = [
-    ('Alex', 'Herman', '1994'),
-    ('Eugene', 'Zaika', '1994')
-]
+app.config['SECRET_KEY'] = 'asd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
+db = SQLAlchemy(app)
 
 
 class AddStudentForm(FlaskForm):
@@ -24,47 +17,44 @@ class AddStudentForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
 
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '({}, {}, {}, {})'.format(self.id, self.first_name, self.last_name, self.year)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def show_all_students():
     form = AddStudentForm()
-    conn = sqlite3.connect('student.db')
-    cur = conn.cursor()
-    cur.execute('SELECT rowid, * FROM students')
-    students_db = cur.fetchall()
+    students_db = Student.query.all()
     amount = len(students_db)
-    cur.execute('SELECT  AVG(year) FROM students')
-    avg = cur.fetchall()
-    avg = '{:.1f}'.format(2020 - avg[0][0])
-    conn.commit()
-    conn.close()
-    return render_template('students.html', students=students_db, form=form, amount=amount, avg=avg)
+    return render_template('students.html', students=students_db, form=form, amount=amount)
 
 
 @app.route('/add/', methods=['POST'])
 def add_student():
     form = AddStudentForm()
     if form.validate_on_submit():
-        conn = sqlite3.connect('student.db')
-        cur = conn.cursor()
-        cur.execute('INSERT INTO students VALUES (?, ?, ?)', (form.first_name.data, form.last_name.data, form.year.data))
-        conn.commit()
-        conn.close()
+        new_student = Student(first_name=form.first_name.data, last_name=form.last_name.data, year=form.year.data)
+        db.session.add(new_student)
+        db.session.commit()
         flash('Студент был добавлен в список', 'success')
     else:
         flash('Invalid input', 'danger')
     return redirect(url_for('show_all_students'))
 
 
-@app.route('/remove/', methods=['POST', 'DELETE'])
+@app.route('/remove/', methods=['DELETE', 'POST'])
 def remove_student():
-    if request.method == 'POST':
-        conn = sqlite3.connect('student.db')
-        cur = conn.cursor()
-        student_id = request.form['delete']
-        cur.execute('DELETE from students WHERE rowid=(?)', (student_id, ))
-        conn.commit()
-        conn.close()
-        flash('Студент был удален из списка', 'warning')
+    student_id = request.form['delete']
+    student_to_delete = db.session.query(Student).filter_by(id=student_id).first()
+    db.session.delete(student_to_delete)
+    db.session.commit()
+    flash('Студент был удален из списка', 'warning')
     return redirect(url_for('show_all_students'))
 
 
