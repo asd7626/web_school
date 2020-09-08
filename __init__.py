bd95acdcb8f41b7d063d1dc6d149cda4ec5d76b5
asd7626 <asdshq19@gmail.com>
2020-09-08 19:19:25 +0300

    Configuration

--- __init__.py
+++ __init__.py
@@ -1,28 +1,18 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from datetime import timedelta
 from flask_mail import Mail
-import os
+from web_school.config import Config
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'asd'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['REMEMBER_COOKIE_DURATION'] = timedelta(hours=1)
-
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'auth.login'
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
@@ -36,7 +26,6 @@ app.register_blueprint(auth_blueprint)
 
 from .main import main as main_blueprint
 app.register_blueprint(main_blueprint)
-
 
 from web_school.models import User, Student
 from web_school import routes
