8fe33eff3e351f376bd6db7beb55add97eab16fb
asd7626 <asdshq19@gmail.com>
2020-09-17 00:13:57 +0300

    Run

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from web_school import app
+from web_school import create_app, db
+import os
+
+app = create_app()
+app.app_context().push()
 
 if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
     app.run(host='localhost', port=1115, debug=True)
