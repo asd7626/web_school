86dd3c3f91050af8680fddf8bffe8e6ae3733a4a
asd7626 <asdshq19@gmail.com>
2020-09-17 00:06:36 +0300

    Subject

---
+++ subject/models.py
@@ -0,0 +1,8 @@
+from web_school import db
+from flask_login import UserMixin
+
+
+class Subject(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    teacher = db.relationship('Teacher', backref='subject')
